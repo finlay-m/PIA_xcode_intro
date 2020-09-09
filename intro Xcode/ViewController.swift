110d23807ed087f77f6139cab01b343300a9e8a4
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-09-09 14:09:45 +0200

    Initial Commit

---
+++ intro Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intro Xcode
+//
+//  Created by Finlay MacGregor on 9/9/20.
+//  Copyright © 2020 Finlay MacGregor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
